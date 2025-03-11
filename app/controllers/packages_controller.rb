4d02842d58323f3ccf3f83ef2ff684e664567478
Micaela February <micaela.february9@gmail.com>
2025-03-11 16:12:52 +0200

    packages actions

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,8 +2,15 @@ class PackagesController < ApplicationController
   def show
     @package = Package.find(params[:id])
   end
-end
 
-def edit
-  @package = Package.find(params[:id])
+  def edit
+    @package = Package.find(params[:id])
+  end
+
+  def update
+    @package = Package.find(params[:id])
+    @package.update(package_params)
+
+    redirect_to package_path(@package)
+  end
 end
