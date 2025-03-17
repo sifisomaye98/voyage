e019ed12cd8c8fe68ce74f0c9941c64758fa0fd8
Sifiso Maye <sifmay98@gmail.com>
2025-03-17 18:01:56 +0200

    doing mapbox

--- app/controllers/destination_controller.rb
+++ app/controllers/destination_controller.rb
@@ -1,6 +1,13 @@
 class DestinationController < ApplicationController
   def index
-    @destinations = Destination.all
+    @destinations = Destination.all # change to current user destinations (has many through)
+    @markers = @destinations.geocoded.map do |destination|
+      {
+        lat: destination.latitude,
+        lng: destination.longitude
+      }
+    end
+
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_14_131513) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_152746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_14_131513) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "flights", force: :cascade do |t|
