2c66a181f3122149745fd62c1b65afbbedc81458
Sifiso Maye <sifmay98@gmail.com>
2025-03-18 15:35:13 +0200

    getting info from ai

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,4 +1,4 @@
-class DestinationController < ApplicationController
+class DestinationsController < ApplicationController
   def index
     @destinations = current_user.destinations # change to current user destinations (has many through)
     @markers = @destinations.geocoded.map do |destination|
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :trips
+        :recoverable, :rememberable, :validatable
   has_many :journals, through: :trips
 end
