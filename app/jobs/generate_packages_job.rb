c0a896a6756365bac96aa674c625c5eeea44f9a3
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-03-19 18:06:37 +0200

    hotwiiiiire and tubo frames

--- app/jobs/generate_packages_job.rb
+++ app/jobs/generate_packages_job.rb
@@ -3,21 +3,23 @@ class GeneratePackagesJob < ApplicationJob
 
   def perform(trip)
     client = OpenAI::Client.new
-    chatgpt_response = client.chat(parameters: {
-      model: "gpt-4o-mini",
-      messages: [{ role: "user", content: "Please generate 1 travel package for  #{trip.category} to #{trip.destination.name}
-        with a budget of #{trip.budget} from #{trip.start_date} to #{trip.end_date}. The packages must be specific to #{trip.category} trips,
-        and they must be related to #{trip.destination.name}. Do not include any packages for other categories or destinations.
-        Don't mention the dates, category, and total budget at the beginning of your answer.Just the package details. Make the description short"}]
-    })
-    response_text = chatgpt_response["choices"][0]["message"]["content"]
+    3.times do
+      chatgpt_response = client.chat(parameters: {
+        model: "gpt-4o-mini",
+        messages: [{ role: "user", content: "Please generate 1 travel package for  #{trip.category} to #{trip.destination.name}
+          with a budget of #{trip.budget} from #{trip.start_date} to #{trip.end_date}. The packages must be specific to #{trip.category} trips,
+          and they must be related to #{trip.destination.name}. Do not include any packages for other categories or destinations.
+          Don't mention the dates, category, and total budget at the beginning of your answer.Just the package details. Make the description short"}]
+      })
+      response_text = chatgpt_response["choices"][0]["message"]["content"]
 
-    package = Package.create!(
-      name: "#{trip.category} package to #{trip.destination.name}",
-      description: response_text,
-      trip_id: trip.id
-    )
-    package.set_photo(trip) if package.respond_to?(:set_photo)
+      package = Package.create!(
+        name: "#{trip.category} package to #{trip.destination.name}",
+        description: response_text,
+        trip_id: trip.id
+      )
+      package.set_photo(trip) if package.respond_to?(:set_photo)
+    end
 
     # Now broadcast so the view updates to show the new package
     trip.broadcast_replace_to(
@@ -26,6 +28,6 @@ class GeneratePackagesJob < ApplicationJob
       partial: "trips/packages",
       locals: { trip: trip }
     )
-    return package
+
   end
 end
