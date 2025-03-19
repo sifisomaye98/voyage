94d8fb751883d89536bfdf7f46061ba07c2608cd
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-03-19 14:33:50 +0200

    flights hotels and itineraries removed, new seed file

--- db/migrate/20250311110207_create_packages.rb
+++ db/migrate/20250311110207_create_packages.rb
@@ -1,8 +1,6 @@
 class CreatePackages < ActiveRecord::Migration[7.1]
   def change
     create_table :packages do |t|
-      t.references :flight, null: false, foreign_key: true
-      t.references :hotel, null: false, foreign_key: true
       t.integer :category
       t.string :name
 
--- db/migrate/20250314115942_change_flight_id_and_hotel_id_to_nullable_in_packages.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFlightIdAndHotelIdToNullableInPackages < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :packages, :flight_id, true
-    change_column_null :packages, :hotel_id, true
-  end
-end
