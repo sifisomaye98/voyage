fa5b5370563016f68d90a4b28288c8be56a70e5f
Sifiso Maye <sifmay98@gmail.com>
2025-03-20 16:56:08 +0200

    removed the initializer

--- config/initializers/solid_queue.rb
+++
@@ -1,5 +0,0 @@
-SolidQueue.configure do |config|
-  # Tells SolidQueue to use the same environment key
-  # e.g. "development" locally, "production" on Heroku
-  config.database_key = Rails.env
-end
